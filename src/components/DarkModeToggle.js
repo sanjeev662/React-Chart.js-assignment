79e059bc6e5198223fd87ef1395907a0c3046a74
sanjeev662 <sanjeevsinghkaushik662@gmail.com>
2024-06-02 08:50:01 +0530

    Fix : Button text change

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -6,7 +6,7 @@ const DarkModeToggle = ({ darkMode, setDarkMode }) => {
       onClick={() => setDarkMode(!darkMode)}
       className="p-2 bg-gray-300 dark:bg-gray-700 text-black dark:text-white rounded"
     >
-      {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+      {darkMode ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
     </button>
   );
 };
